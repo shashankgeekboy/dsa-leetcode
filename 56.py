b2bef67fb5cdd37d2bd3dec5352a150f87903d3c
codewithgeekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-09-15 23:53:29 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,18 @@
+class Solution:
+  def minCostConnectPoints(self, points: List[int]) -> int:
+    
+    dist = [math.inf] * len(points)
+    ans = 0
+
+    for i in range(len(points) - 1):
+      for j in range(i + 1, len(points)):
+        
+        dist[j] = min(dist[j], abs(points[i][0] - points[j][0]) +
+                      abs(points[i][1] - points[j][1]))
+        
+        if dist[j] < dist[i + 1]:
+          points[j], points[i + 1] = points[i + 1], points[j]
+          dist[j], dist[i + 1] = dist[i + 1], dist[j]
+      ans += dist[i + 1]
+
+    return ans
