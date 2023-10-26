a6a1dc3dc834fd3eddc67e2364eddb485acca83f
codewithgeekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-10-26 22:23:42 +0530

    Create 82.py

---
+++ 82.py
@@ -0,0 +1,18 @@
+class Solution:
+  def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+    kMod = 1_000_000_007
+    n = len(arr)
+    
+    dp = [1] * n
+    arr.sort()
+    numToIndex = {a: i for i, a in enumerate(arr)}
+
+    for i, root in enumerate(arr):  
+      for j in range(i):
+        if root % arr[j] == 0:  
+          right = root // arr[j]
+          if right in numToIndex:
+            dp[i] += dp[j] * dp[numToIndex[right]]
+            dp[i] %= kMod
+
+    return sum(dp) % kMod
