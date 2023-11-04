72bf3034cf4d06a844b93efea501e7c6237a6b15
codewithgeekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-11-04 20:17:05 +0530

    Create 90.java

---
+++ 90.java
@@ -0,0 +1,8 @@
+
+class Solution {
+    public int getLastMoment(int n, int[] left, int[] right) {
+        int maxLeft = Arrays.stream(left).max().orElse(0);
+        int minRight = n - Arrays.stream(right).min().orElse(n);
+        return Math.max(maxLeft, minRight);
+    }
+}
