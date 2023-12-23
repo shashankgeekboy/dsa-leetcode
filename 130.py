450d10508bb1faecc6c2e00425256e5635399a44
codewithgeekboy <113760650+shashankgeekboy@users.noreply.github.com>
2023-12-23 15:02:03 +0530

    Create 130.py

---
+++ 130.py
@@ -0,0 +1,17 @@
+
+class Solution:
+   
+    def isPathCrossing(self, path: str) -> bool:
+        visited = set()
+        x = y = 0
+        for di in path:
+            visited.add((x, y))
+            if di == "N": y += 1
+            elif di == "S": y -= 1
+            elif di == "E": x += 1
+            elif di == "W": x -= 1
+            if (x, y) in visited:
+                return True
+        return False
+
+
